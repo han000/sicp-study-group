acdc4c9e2529a310e933e57f248e5a74ebacb85d
Hanif Jameel <hanif_1989@btinternet.com>
2013-03-23 12:08:20 +0000

    use existing definition of equal

--- helpers.rkt
+++ helpers.rkt
@@ -1,10 +1,6 @@
 #lang racket
 
-(define (equal-lists? list1 list2)
-  (cond ((and (null? list1) (null? list2)) #t)
-        ((or (null? list1) (null? list2)) #f)
-        ((= (car list1) (car list2)) (equal-lists? (cdr list1) (cdr list2)))
-        (else #f)))
+(define equal-lists? equal?)
 
 (define (square x)
   (* x x))
